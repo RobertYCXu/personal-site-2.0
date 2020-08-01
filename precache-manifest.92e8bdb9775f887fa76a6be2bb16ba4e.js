0d4b40b7fac0d3cd32d49f679b5c2217567ac763
Robert Xu <robertyc.xu@gmail.com>
2020-08-01 01:46:13 -0400

    Updates

---
+++ precache-manifest.92e8bdb9775f887fa76a6be2bb16ba4e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecddb5cd69a24a26e0015c6e21f80a36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db6165dec8e8468bee06",
+    "url": "/static/js/2.20d53328.chunk.js"
+  },
+  {
+    "revision": "2ea4cad06ecf4803d714cb80b9fc4261",
+    "url": "/static/js/2.20d53328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693f1323f677bbe981c8",
+    "url": "/static/js/main.3a3db848.chunk.js"
+  },
+  {
+    "revision": "f85d4943d3d0951e8416",
+    "url": "/static/js/runtime-main.20513170.js"
+  },
+  {
+    "revision": "fc23d92814d44195e94ebb0d3b23ef6c",
+    "url": "/static/media/0.fc23d928.jpg"
+  },
+  {
+    "revision": "d736f23a47f5781cf682bca22335c95e",
+    "url": "/static/media/1.d736f23a.jpg"
+  },
+  {
+    "revision": "051ec6a6d5455b029e936f2e8f93f8a1",
+    "url": "/static/media/2.051ec6a6.jpg"
+  },
+  {
+    "revision": "35ef735724bd58a03c7b1f4f519645c1",
+    "url": "/static/media/3.35ef7357.jpg"
+  },
+  {
+    "revision": "05d9f8b676af0f60061f5f39abf7878f",
+    "url": "/static/media/4.05d9f8b6.jpg"
+  },
+  {
+    "revision": "ffb4d60febe5fd4e77371dd7d4fcc6d8",
+    "url": "/static/media/5.ffb4d60f.jpg"
+  },
+  {
+    "revision": "374ec1f3d0fd64039375a5b9446c5e4b",
+    "url": "/static/media/6.374ec1f3.jpg"
+  },
+  {
+    "revision": "092b0d11d08f8d69756d4b8c5417f6e6",
+    "url": "/static/media/7.092b0d11.jpg"
+  },
+  {
+    "revision": "34b3692d2d3c6e03aaa90d330e1b99e6",
+    "url": "/static/media/8.34b3692d.jpg"
+  },
+  {
+    "revision": "98b98d3d446107a577daefa19dae9849",
+    "url": "/static/media/i10.98b98d3d.jpg"
+  },
+  {
+    "revision": "01b2c13310e87c69d7634baf158093c2",
+    "url": "/static/media/i11.01b2c133.jpg"
+  },
+  {
+    "revision": "da674292e9a9f338e7a95d5f75428885",
+    "url": "/static/media/profile.da674292.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04717442a9a68e97b45bbfa537f8723c.js"
+  "/precache-manifest.92e8bdb9775f887fa76a6be2bb16ba4e.js"
 );
 
 self.addEventListener('message', (event) => {
