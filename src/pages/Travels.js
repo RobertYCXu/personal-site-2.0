d5ced40d2ed0536ce913766188a71f4a481231f6
Robert Xu <robertyc.xu@gmail.com>
2020-08-08 01:36:34 -0400

    Changed photos to progressive JPG and added travel menu

--- src/pages/Travels.js
+++ src/pages/Travels.js
@@ -9,9 +9,43 @@ const StyledTravels = styled.div`
   justify-content: center;
   align-items: center;
 
-  .photo-container {
-    padding: 10%;
+  .content-container {
+    display: flex;
+    justify-content: flex-end;
   }
+
+  .photos-menu {
+    display: flex;
+    align-items: flex-end;
+    flex-direction: column;
+    padding-top: 7%;
+  }
+
+  .fixed-menu {
+    position: fixed;
+  }
+
+  .country {
+    text-align: right;
+    letter-spacing: 2px;
+    line-height: 2;
+    color: grey;
+    cursor: pointer;
+    transition: color 200ms ease-in-out;
+  }
+
+  .country:hover {
+    color: ${({ theme }) => theme.primaryAccent};
+  }
+
+  .photos-container {
+    max-width: 80%;
+  }
+
+  .photo-container {
+    padding: 10% 10% 10% 2%;
+  }
+
   img {
     max-width: 100%;
     max-height: 100%;
@@ -20,6 +54,7 @@ const StyledTravels = styled.div`
 `;
 
 const numImgs = 9;
+const countries = ["iceland", "hawaii", "san francisco", "new york", "toronto", "japan"]
 
 const imgs = [...Array(numImgs).keys()].map(i => {
   return(
@@ -37,7 +72,16 @@ function Travels() {
           <h1>travels</h1>
         </div>
         <div className="content-container">
-          { imgs }
+          <div className="photos-menu">
+            <div className="fixed-menu">
+              {countries.map(country => (
+                <div className="country">{country}</div>
+              ))}
+            </div>
+          </div>
+          <div className="photos-container">
+            { imgs }
+          </div>
         </div>
       </div>
     </StyledTravels>
